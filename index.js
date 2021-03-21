45efef51abef32ff4d522d160c728d4052507550
Xiaochen Xu <xiaochex@gmail.com>
2021-03-20 21:54:20 -0400

    commit tree 0e4db0a4910c7a8c40b8559e1f96b42e80569705

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+function superbowlWin(records){
+    let found = records.find((record)=>{
+        
+        return record.result === 'W'
+            
+        
+    })
+//console.log(found)
+if (found){
+    return found.year;
+};
+
+ }
+ 
+ l
